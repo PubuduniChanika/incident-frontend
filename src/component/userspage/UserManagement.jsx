5b48deb87d6d02797f52f98f1ef74dd775ba4c8c
PubuduniChanika <pchanikawijesinghe@gmail.com>
2024-10-29 15:00:06 +0530

    enhanced user management interface

--- src/component/userspage/UserManagement.jsx
+++ src/component/userspage/UserManagement.jsx
@@ -1,21 +1,69 @@
 // components/UserManagementPage.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import UserService from '../service/UserService';
+import { createColumnHelper, useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function UserManagementPage() {
   const [users, setUsers] = useState([]);
 
+  const columnHelper = createColumnHelper();
+
+  const columns = useMemo(() => [
+    columnHelper.accessor('id', {
+      header: 'Id',
+      enableColumnFilter: false,
+      enableSorting: false,
+      cell: () => null, // This will hide the cell content
+    }),
+    columnHelper.accessor('name', { header: 'Name' }),
+    columnHelper.accessor('email', { header: 'Email' }),
+    columnHelper.display({
+      id: 'action',
+      header: 'Action',
+      cell: ({ row }) => (
+        <div className="flex space-x-2">
+          <Link to={`/update-user/${row.original.id}`}>
+            <IconButton 
+              color="primary" 
+              aria-label="update" // Accessibility label
+            >
+              <EditIcon /> {/* Material-UI edit icon */}
+            </IconButton>
+          </Link>
+          
+          <IconButton 
+            color="error" 
+            onClick={() => deleteUser(row.original.id)}
+            aria-label="delete" // Accessibility label
+          >
+            <DeleteIcon /> {/* Material-UI delete icon */}
+          </IconButton>
+
+          
+        </div>
+      ),
+    }),
+  ], []);
+
+  const table = useReactTable({
+    data: users,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   useEffect(() => {
-    // Fetch users data when the component mounts
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
     try {
-      const token = localStorage.getItem('token'); // Retrieve the token from localStorage
+      const token = localStorage.getItem('token');
       const response = await UserService.getAllUsers(token);
-      setUsers(response.systemUsersList); // Assuming the list of users is under the key 'systemUsersList'
+      setUsers(response.systemUsersList);
     } catch (error) {
       console.error('Error fetching users:', error);
     }
@@ -23,13 +71,10 @@ function UserManagementPage() {
 
   const deleteUser = async (userId) => {
     try {
-      // Prompt for confirmation before deleting the user
       const confirmDelete = window.confirm('Are you sure you want to delete this user?');
-
-      const token = localStorage.getItem('token'); // Retrieve the token from localStorage
+      const token = localStorage.getItem('token');
       if (confirmDelete) {
         await UserService.deleteUser(userId, token);
-        // After deleting the user, fetch the updated list of users
         fetchUsers();
       }
     } catch (error) {
@@ -39,47 +84,49 @@ function UserManagementPage() {
 
   return (
     <div className="container mx-auto p-6">
-      <h2 className="text-2xl font-bold mb-4">Users Management Page</h2>
-      <Link to="/register">
-        <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow hover:bg-blue-700 transition duration-200">
-          Add User
-        </button>
-      </Link>
-      <div className="overflow-x-auto mt-4">
-        <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-lg">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="py-3 px-4 border-b border-gray-300 text-left text-gray-600 font-medium">ID</th>
-              <th className="py-3 px-4 border-b border-gray-300 text-left text-gray-600 font-medium">Name</th>
-              <th className="py-3 px-4 border-b border-gray-300 text-left text-gray-600 font-medium">Email</th>
-              <th className="py-3 px-4 border-b border-gray-300 text-left text-gray-600 font-medium">Actions</th>
-            </tr>
+      <h2 className="text-2xl font-bold mb-4 text-center">Users Management Page</h2>
+      <div className="flex justify-end mb-4">
+        <Link to="/register">
+          <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-700 transition duration-200">
+            Add User
+          </button>
+        </Link>
+      </div>
+      <div className="overflow-x-auto rounded-lg shadow-lg mt-4 w-1/2 mx-auto bg-white p-4"> {/* Added background and padding */}
+        <table className="min-w-full bg-white border border-gray-200 rounded-lg"> {/* Added rounded corners */}
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id} className="bg-gray-100 text-gray-600">
+                {headerGroup.headers.map(header => {
+                  if (header.id !== 'id') {
+                    return (
+                      <th key={header.id} className="py-3 px-6 text-left text-sm font-medium uppercase tracking-wider">
+                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      </th>
+                    );
+                  }
+                  return null;
+                })}
+              </tr>
+            ))}
           </thead>
           <tbody>
-            {users.length > 0 ? (
-              users.map(user => (
-                <tr key={user.id} className="border-b border-gray-300 hover:bg-gray-50 transition duration-200">
-                  <td className="py-3 px-4 text-gray-700">{user.id}</td>
-                  <td className="py-3 px-4 text-gray-700">{user.name}</td>
-                  <td className="py-3 px-4 text-gray-700">{user.email}</td>
-                  <td className="py-3 px-4 flex space-x-2">
-                    <button 
-                      className="bg-red-600 text-white font-semibold py-1 px-2 rounded-lg hover:bg-red-700 transition duration-200" 
-                      onClick={() => deleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                    <Link to={`/update-user/${user.id}`}>
-                      <button className="bg-yellow-500 text-white font-semibold py-1 px-2 rounded-lg hover:bg-yellow-600 transition duration-200">
-                        Update
-                      </button>
-                    </Link>
-                  </td>
+            {table.getRowModel().rows.length > 0 ? (
+              table.getRowModel().rows.map(row => (
+                <tr key={row.id} className="border-b border-gray-300 hover:bg-gray-50 transition duration-200">
+                  {row.getVisibleCells().map(cell => {
+                    if (cell.column.id !== 'id') {
+                      return (
+                        <td key={cell.id} className="py-3 px-4 text-gray-700">{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                      );
+                    }
+                    return null;
+                  })}
                 </tr>
               ))
             ) : (
               <tr>
-                <td colSpan="4" className="text-center py-4 text-gray-500">No users found.</td>
+                <td colSpan="3" className="text-center py-4 text-gray-500">No users found.</td>
               </tr>
             )}
           </tbody>
